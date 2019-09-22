77fd89fead9ca47d4ceda2bfd197480a23faa9cb
etresoft <jdaniel@etresoft.com>
2019-09-22 18:42:20 -0400

    Refactor to framework.

---
+++ ESHelp/ESHelpKit/ESHelpKit.h
@@ -0,0 +1,14 @@
+/***********************************************************************
+ ** Etresoft, Inc.
+ ** Copyright (c) 2019. All rights reserved.
+ **********************************************************************/
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for ESHelp.
+FOUNDATION_EXPORT double ESHelpVersionNumber;
+
+//! Project version string for ESHelp.
+FOUNDATION_EXPORT const unsigned char ESHelpVersionString[];
+
+#import <ESHelpKit/ESHelp.h>
--- ESHelpDemo/ESHelpDemo/AppDelegate.m
+++ ESHelpDemo/ESHelpDemo/AppDelegate.m
@@ -4,7 +4,7 @@
  **********************************************************************/
 
 #import "AppDelegate.h"
-#import "ESHelp.h"
+#import <ESHelpKit/ESHelpKit.h>
 
 @interface AppDelegate ()
 
